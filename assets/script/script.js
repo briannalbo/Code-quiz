c3e384412560cf6cf9c2f1d62a524175babe28ec
unknown <brianna.albo@gmail.com>
2022-11-07 23:28:11 -0500

    adds a timer that will be triggered by start button; updates some css styling

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,8 +2,31 @@ var firstCommit;
 firstCommit = "Coders";
 console.log(firstCommit + " welcome");
 
-function stopWatch(){
-    var timer = timer.setInterval(function () => {
+var startBtn = document.querySelector("#start-button");
+
+var clock = document.getElementById("timeleft");
+var secondsLeft = 90;
+
+function remainingTime() {
+    var timer = setInterval(function () {
+
+        secondsLeft--;
+        clock.textContent = secondsLeft + " s";
+
+        if (secondsLeft <= 0){
+            clearInterval(clock);
+            clock.textContent = "Time is up!";
+        }
+        else {
+            
+
+        }
     
-}, interval);
+}, 1000);
 }
+
+function startQuiz() {
+    remainingTime();
+}
+
+startBtn.addEventListener("click", startQuiz);
