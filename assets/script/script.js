9f1c11b35cdd9fc194d593e0f4c12d30df54bea3
unknown <brianna.albo@gmail.com>
2022-11-10 18:29:11 -0500

    this is a mess

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,48 +9,50 @@ var answerBtn1 = document.querySelector("#answer-btn1");
 var answerBtn2 = document.querySelector("#answer-btn2");
 var answerBtn3 = document.querySelector("#answer-btn3");
 var answerBtn4 = document.querySelector("#answer-btn4");
-var answerButtons = document.querySelector(.choices-button);
+var answerButtons = document.querySelector(".choices-button");
+var quiz = document.querySelector("#quiz-area");
+var answerCheck = document.querySelector("#answer-check")
 
 var quizQuestions = [
     {
-        question: "Which part of the HTML is the JavaScript linked in?",
+        quest: "Which part of the HTML is the JavaScript linked in?",
         choices: ["a. Head", "b. Body", "c. Footer", "d. Nav"],
         answer: "b. Body"
     },
     {
-        question: "Which of the following is not a commonly used data type?",
+        quest: "Which of the following is not a commonly used data type?",
         choices: ["a. Booleans", "b. Strings", "c. Numbers", "d. Element ID"],
         answer: "d. Element ID"
     },
     {
-        question: "What is the first index in an array?",
+        quest: "What is the first index in an array?",
         choices: ["a. 1", "b. 0", "c. -1", "d. 10"],
         answer: "b. 0"
     },
     {
-        question: "Which operator is used to assign a value to a variable in JavaScript?",
+        quest: "Which operator is used to assign a value to a variable in JavaScript?",
         choices: ["a. #", "b. =", "c. ^ ", "d. * "],
         answer: "b. = "
     },
     {
-        question: "How do you call a function called 'myFunction'?",
+        quest: "How do you call a function called 'myFunction'?",
         choices: ["a. use = myFunction", "b. function == myFunction", "c. myFunction()", "d. call myFunction()"],
         answer: "c. myFunction()"
     },
     {
-        question: "How can you add a comment in JavaScript?",
+        quest: "How can you add a comment in JavaScript?",
         choices: ["a. //This is a comment", "b. <!--This is a comment-->", "c. 'This is a comment'", "d. **This is a comment"],
         answer: "a. //This is a comment"
     },
     {
-        question: "Which event occurs when the user clicks on an HTML element?",
+        quest: "Which event occurs when the user clicks on an HTML element?",
         choices: ["a. touch", "b. onMouse", "c. onTouch", "d. onclick"],
         answer: "d. onclick"
     },
     {
-        question: "How do you declare a variable in JavaScript?",
-        choices: ["a. v = myVariable", "b. v myVariable", "c. var myVariable", "d. variable = myVariable"],
-        answer: "c. var myVariable"
+        quest: "How do you declare a variable in JavaScript?",
+        choices: ["a. v e", "b. v my", "c. va", "d. v"],
+        answer: "c. vaVariable"
     }
 ];
 
@@ -75,21 +77,49 @@ function remainingTime() {
     
 }, 1000);
 }
+var questionNumber = 0;
 
-function showQuestion(n) {
+function showQuestion() {
+   
     
-    askQuestion.textContent = quizQuestions[n].question;
-    answerBtn1.textContent = quizQuestions[n].choices[0];
-    answerBtn2.textContent = quizQuestions[n].choices[1];
-    answerBtn3.textContent = quizQuestions[n].choices[2];
-    answerBtn4.textContent = quizQuestions[n].choices[3];
-    questionNumber = n;
+   var question = quizQuestions[questionNumber];
+   var questionNumber = 0;
+   
+    askQuestion.textContent = quizQuestions[questionNumber].quest[0];
+    answerBtn1.textContent = quizQuestions[questionNumber].choices[0];
+    answerBtn2.textContent = quizQuestions[questionNumber].choices[1];
+    answerBtn3.textContent = quizQuestions[questionNumber].choices[2];
+    answerBtn4.textContent = quizQuestions[questionNumber].choices[3];
+    
 }
 
+    answerButtons.onclick = () => {
+        if (questionNumber < quizQuestions.length - 1) {
+            showQuestion();
+            ++questionNumber;
+            
+    }
+    else {
+        alert("error");
+    }
+
+
+}
+
+
 function startQuiz() {
-    showQuestion(questionNumber);
+    showQuestion();
     remainingTime();
-    questionNumber = 0;
+    
+}
+
+var questionCount = 0;
+
+
+
+function gameOver() {
+quiz.style.display = "none";
 }
 
 startBtn.addEventListener("click", startQuiz);
+// answerButtons.addEventListener("click", );
